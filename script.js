9e977db696abf123a8d68138384cb198369f91a8
Harshal Atkar <142751341+HarshalAtkar@users.noreply.github.com>
2023-12-06 14:29:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
 const makeChange = (c) => {
   // your name here
+	let p=0,n=0,d=0,q=0;
+	if(c>=25)
+	{
+		c=c-25;
+		q++;
+	}
+	else if(c<25)
+	{
+		c=c-10;
+		d++;
+	}
+	else if(c<10)
+	{
+		c=c-5;
+		n++;
+	}
+	if(c<5)
+	{
+		c=c-1;
+		p++;
+	}
+	let ans={ "q": , "d": , "n": , "p":  };
+	return ans;
+	
 };
 
 // Do not the change the code below
-const c = prompt("Enter c: ");
+// const c = prompt("Enter c: ");
 alert(JSON.stringify(makeChange(c)));
